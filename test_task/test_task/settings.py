7291db42c9f0c373078c5c52404d66791ac45896
German <coolmatoff@gmail.com>
2022-01-08 18:46:09 +0600

    changed db to postgres

--- test_task/test_task/settings.py
+++ test_task/test_task/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'test_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
 
